f789d30c7cf9c2ce4b566c0e5c0e2698e496e9d5
RVerma90 <irahulverma90@gmail.com>
2017-10-12 23:57:50 +0100

    Retrieves Crypto prices of some coins and shoind in $,£,€

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+		super(props);
+		
+		this.state = {
+			cryptos: []
+		};
+  }
+
+  componentDidMount() {
+		axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT,XRP,LTC,OMG,NEO&tsyms=BTC,GBP,USD,EUR')
+			.then(res => {
+				const cryptos = res.data;
+				console.log(cryptos);
+				this.setState({cryptos: cryptos});
+			})
+	}
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+			<div className="App">
+				{Object.keys(this.state.cryptos).map((key) => (
+
+					<div id="crypto-container">
+						<span className="left">{key}</span>
+						<span className="prices">${this.state.cryptos[key].USD}</span>
+						<span className="prices">£{this.state.cryptos[key].GBP}</span>
+						<span className="prices">€{this.state.cryptos[key].EUR}</span>
+					</div>
+
+				))}
+
+			</div>
     );
   }
 }
